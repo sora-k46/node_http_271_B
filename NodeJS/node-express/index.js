371e75da345a8ab7303376fe2883501fcc0d7e11
Sorrawit <sorrawit456789@gmail.com>
2022-05-02 17:04:45 +0700

    BO4 Express Serve Static Files

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -3,6 +3,11 @@ const hostname = 'localhost';
 const port = 3000;
 const app = express();
 
+const morgan = require('morgan');
+
+app.use(morgan('dev')); 
+app.use(express.static(__dirname+'/public'));
+
 app.use((req, res, next) => {
   console.log(req.headers);
   res.statusCode = 200;
